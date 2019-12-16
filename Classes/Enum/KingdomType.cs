6fa658eabb61480407c336a84827d653ceb127bf
Zelkior <rojon92@gmail.com>
2019-12-16 01:04:16 +0100

    Ajout début nom royaume

---
+++ Classes/Enum/KingdomType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldGen.Classes.Enum
+{
+    public enum KingdomType
+    {
+        Royaume,        // Dirigeant: Roi,                  Nom: Royaume de ----
+        Empire,         // Dirigeant: Empereur,             Nom: L'empire <Nom de la race>
+        Théocratie,     // Dirigeant: Grand pêtre/prophéte, Nom: L'ordre de <Nom de dieu> OU Nom aléatoire
+        Polysynodie     // Dirigeant: Le conseil,           Nom: Nom aléatoire
+    }
+}
